da4e5d9c65966ced2bc83c34df47976b16bce235
obai jar <obaijar@gmail.com>
2024-07-03 14:33:33 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 SharedPreferences? prefs;
 
-void main() async {
+void main()  async {
   WidgetsFlutterBinding.ensureInitialized();
   prefs = await SharedPreferences.getInstance();
   runApp(
