4e4fd42018378a87a1c3b8f37d17882a7b49b805
obai jar <obaijar@gmail.com>
2024-07-22 09:52:08 +0300

    new

--- lib/screens/teachersReg.dart
+++ lib/screens/teachersReg.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:dio/dio.dart';
 
 class TeacherReg extends StatefulWidget {
   @override
--- lib/screens/videoPost.dart
+++ lib/screens/videoPost.dart
@@ -1,11 +1,11 @@
-// ignore_for_file: file_names, depend_on_referenced_packages, use_key_in_widget_constructors, library_private_types_in_public_api, avoid_init_to_null, avoid_print
-
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class VideoPost extends StatefulWidget {
   @override
@@ -14,6 +14,70 @@ class VideoPost extends StatefulWidget {
 
 class _VideoPostState extends State<VideoPost> {
   File? _videoFile;
+  final TextEditingController _titleController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? _selectedGrade;
+  String? _selectedSubject;
+  String? _selectedSubjectType;
+  String? _selectedTeacher;
+
+  final Map<String, String> _grades = {
+    'تاسع': '9',
+    'بكالوريا علمي': '12',
+    'بكالوريا أدبي': '11'
+  };
+
+  final Map<String, String> _subjects = {
+    'رياضيات': 'math',
+    'علوم': 'science',
+    'تاريخ': 'history'
+  };
+
+  final Map<String, String> _subjectTypes = {
+    'Lecture': '1',
+    'Tutorial': '2',
+    'Lab': '3'
+  };
+
+  List<String> _teachers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTeachers();
+  }
+
+  Future<void> fetchTeachers() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    final url = Uri.parse('https://obai.aunakit-hosting.com/api/teachers/');
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          HttpHeaders.authorizationHeader: 'Token $token',
+        },
+      );
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        setState(() {
+          _teachers = data
+              .map((teacher) => teacher['name'].toString())
+              .toSet()
+              .toList(); // Ensure unique names
+          if (_teachers.isNotEmpty && _selectedTeacher == null) {
+            _selectedTeacher = _teachers
+                .first; // Set the first teacher as default if none is selected
+          }
+        });
+      } else {
+        print('Failed to fetch teachers');
+      }
+    } catch (e) {
+      print('Error during fetching teachers: $e');
+    }
+  }
 
   Future<void> pickVideo() async {
     final pickedFile =
@@ -26,18 +90,25 @@ class _VideoPostState extends State<VideoPost> {
   }
 
   Future<void> uploadVideo(File videoFile) async {
-    final url = Uri.parse('YOUR_UPLOAD_URL_HERE'); // Update this URL
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+    final url =
+        Uri.parse('https://obai.aunakit-hosting.com/api/videos_upload/');
     final request = http.MultipartRequest('POST', url);
+    request.headers[HttpHeaders.authorizationHeader] = 'Bearer $token';
     request.files
         .add(await http.MultipartFile.fromPath('video', videoFile.path));
+    request.fields['title'] = _titleController.text;
+    request.fields['grade'] = _grades[_selectedGrade]!;
+    request.fields['subject'] = _subjects[_selectedSubject]!;
+    request.fields['subject_type'] = _subjectTypes[_selectedSubjectType]!;
+    request.fields['teacher'] = _selectedTeacher!;
 
     try {
       final response = await request.send();
       if (response.statusCode == 200) {
-        // Upload successful
         print('Video uploaded successfully');
       } else {
-        // Upload failed
         print('Failed to upload video');
       }
     } catch (e) {
@@ -54,38 +125,163 @@ class _VideoPostState extends State<VideoPost> {
           style: TextStyle(fontSize: 20.sp),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: pickVideo,
-              child: Text(
-                'إختيار درس',
-                style: TextStyle(fontSize: 15.sp, color: Colors.blue),
-              ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                    labelText: 'العنوان',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'الرجاء إدخال العنوان';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                DropdownButtonFormField<String>(
+                  value: _selectedGrade,
+                  items: _grades.keys.map((grade) {
+                    return DropdownMenuItem(
+                      value: grade,
+                      child: Text(grade),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedGrade = newValue;
+                      print('Selected grade value: ${_grades[_selectedGrade]}');
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'الصف',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null) {
+                      return 'الرجاء اختيار الصف';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                DropdownButtonFormField<String>(
+                  value: _selectedSubject,
+                  items: _subjects.keys.map((subject) {
+                    return DropdownMenuItem(
+                      value: subject,
+                      child: Text(subject),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedSubject = newValue;
+                      print(
+                          'Selected subject value: ${_subjects[_selectedSubject]}');
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'المادة',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null) {
+                      return 'الرجاء اختيار المادة';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                DropdownButtonFormField<String>(
+                  value: _selectedSubjectType,
+                  items: _subjectTypes.keys.map((subjectType) {
+                    return DropdownMenuItem(
+                      value: subjectType,
+                      child: Text(subjectType),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedSubjectType = newValue;
+                      print(
+                          'Selected subject type value: ${_subjectTypes[_selectedSubjectType]}');
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'نوع الدروس',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null) {
+                      return 'الرجاء الاختيار';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                DropdownButtonFormField<String>(
+                  value: _selectedTeacher,
+                  items: _teachers.map((teacher) {
+                    return DropdownMenuItem(
+                      value: teacher,
+                      child: Text(teacher),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedTeacher = newValue;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'المدرس',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null) {
+                      return 'الرجاء اختيار المدرس';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: pickVideo,
+                  child: Text(
+                    'إختيار درس',
+                    style: TextStyle(fontSize: 15.sp, color: Colors.blue),
+                  ),
+                ),
+                if (_videoFile != null)
+                  Text(
+                    '   :الملف المختار ${_videoFile!.path}',
+                    style: TextStyle(fontSize: 15.sp),
+                  ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate() &&
+                        _videoFile != null) {
+                      uploadVideo(_videoFile!);
+                    } else {
+                      print('Please complete all fields and select a video');
+                    }
+                  },
+                  child: Text(
+                    'رفع الدرس',
+                    style: TextStyle(fontSize: 15.sp, color: Colors.blue),
+                  ),
+                ),
+              ],
             ),
-            if (_videoFile != null)
-              Text(
-                '   :الملف المختار ${_videoFile!.path}',
-                style: TextStyle(fontSize: 15.sp),
-              ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_videoFile != null) {
-                  uploadVideo(_videoFile!);
-                } else {
-                  // Show an error message or handle no selected file case
-                  print('No video selected');
-                }
-              },
-              child: Text(
-                'رفع الدرس',
-                style: TextStyle(fontSize: 15.sp, color: Colors.blue),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
